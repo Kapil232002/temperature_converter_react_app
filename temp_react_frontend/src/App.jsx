d94b599ee28e95669260d46cecab2ecd92774adf
kapil kumar <kapilkumar232222@gmail.com>
2025-10-15 00:21:00 +0530

    fix css

--- temp_react_frontend/src/App.jsx
+++ temp_react_frontend/src/App.jsx
@@ -1,38 +1,42 @@
-import React from 'react'
-
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-
   return (
-          <>
-            <div id="landing_page">
-              <div id="navbar">
-                <h1 id="nav-heading">
-                  Temperature<span>Scaler</span>
-                </h1>
-
-                <div className="card">
-                  <div className="card1">
-                    <span>Temperature</span>
-                  </div>
-
-                  <div className="card2">
-                         {/* <label htmlFor="temperature" className="label-text">Enter Temperature:</label> */}
-                         <input type="text" id="x" placeholder="Enter degrees" className="label-text"></input>
-                         {/* <input type="text" id="temperature" name="temperature" /> */}
-                  </div>
-                </div>
-              </div>
-
-              <div id="container">
-                <div className="card3"></div>
-                <div className="card3"></div>
-              </div>
+    <>
+      <div id="landing_page">
+        <div id="navbar">
+          <h1 id="nav-heading">
+            Temperature<span>Scaler</span>
+          </h1>
+          <div className="card">
+            <div className="card1">
+              <span>Temperature</span>
             </div>
-          </>
-
-  )
+            <div className="card2">
+              <input
+                type="text"
+                id="x"
+                placeholder="Enter degrees"
+                className="label-text"
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div id="container">
+          <div className="card-with-heading">
+            <h3 className="card-heading">From</h3>
+            <div className="card3"></div>
+          </div>
+          <div className="card-with-heading">
+            <h3 className="card-heading">To</h3>
+            <div className="card3"></div>
+          </div>
+                  
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
